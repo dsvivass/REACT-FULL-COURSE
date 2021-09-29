d2127f1102f12c22e527c954a98dc7ee297a9fb2
dsvivass <dsvivass@unal.edu.co>
2021-09-29 17:24:55 -0500

    Hot reload fixed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'; // el import es de ES6 JS no es funcionalidad de REACT
 import ReactDOM from 'react-dom'
 
-const Greeting = () => { // Los componentes los debo poner en mayuscula,
+const BookList = () => { // Los componentes los debo poner en mayuscula,
                       // asi react sabe que es especial esta funcion
   
   // Esto que devuelvo es JSX y NO html
@@ -9,54 +9,36 @@ const Greeting = () => { // Los componentes los debo poner en mayuscula,
     // wont work. It must have a parent
   return (
     // En vez de div podemos usar react fragments, o usar semantica de html5
-    <React.Fragment> 
-      <div>
-        <h4>Hello world2!!</h4>    
-        <ul>
-          <li>
-            <a href="#">Hello people</a>
-          </li>
-        </ul>
-      </div>
-    </React.Fragment>
+    
+    <section>
+      <Book/>
+      <Book/>
+      <Book/>
+      <Book/>
+      <Book/>
+      <Book/>
+      <Book/>
+      <Book/>
+      <Book/>
+      <Book/>
+      <Book/>
+    </section>
     
   )
 }
 
 // Usando nested components
 
-const PersonGreeting = () => {
-
+const Book = () => {
   return (
-  <div>
-    <Person/>
-    <Message/>
-  </div>
+    <article>
+      This is a book!
+    </article>
   )
-
 }
 
-const Person = () => <h1>Andres</h1>
-
-const Message = () => <p>Bienvenido!!!!</p>
-
-
-// El equivalente, pero esto no gusta tanto, porque se vuelve tedioso
-// const Greeting = () => {
-//   return React.createElement(
-//     'div', 
-//     {}, 
-//     React.createElement('h4', {}, 'Hello world!!!')
-//   )
-// }
-
-
 ReactDOM.render(
-  <div>
-    <Greeting/>
-    <PersonGreeting/>
-  </div>
-  ,
+  <BookList/>,
   document.getElementById('root') // Aqui es donde estara alojada nuestra app
                                   // esta en /public/index.html
 )
